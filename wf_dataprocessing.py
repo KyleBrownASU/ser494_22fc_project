84a0a01d13da01f4363d34582996aeb115e2af1e
KyleBrownASU <97472449+KyleBrownASU@users.noreply.github.com>
2022-10-23 14:50:26 -0700

    Update wf_dataprocessing.py

--- wf_dataprocessing.py
+++ wf_dataprocessing.py
@@ -1,18 +1,22 @@
 import pandas as pd
 
-us_gdp_growth_exl = "data_original\\us-gdp-growth.xlsx"
+def get_gdp_data():
 
-us_gdp_growth = pd.read_excel(us_gdp_growth_exl)
+    us_gdp_growth_exl = "data_original\\us-gdp-growth.xlsx"
 
-list = us_gdp_growth.values
+    us_gdp_growth = pd.read_excel(us_gdp_growth_exl)
 
-years_list = list[0]
-years_list = years_list.tolist()
-years_list.remove('United States')
+    list = us_gdp_growth.values
 
-values_list = list[1]
-values_list = values_list.tolist()
-del values_list[0]
+    years_list = list[0]
+    years_list = years_list.tolist()
+    years_list.remove('United States')
 
-print(years_list)
-print(values_list)
+    values_list = list[1]
+    values_list = values_list.tolist()
+    del values_list[0]
+
+    return years_list, values_list
+
+    print(years_list)
+    print(values_list)
