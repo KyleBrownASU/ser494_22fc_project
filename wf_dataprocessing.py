f684711e598e7346dc4f7337fc14e378c88fb6bb
KyleBrownASU <97472449+KyleBrownASU@users.noreply.github.com>
2022-10-23 14:14:47 -0700

    make get_gdp_data function

--- wf_dataprocessing.py
+++ wf_dataprocessing.py
@@ -2,6 +2,17 @@ import pandas as pd
 
 us_gdp_growth_exl = "data_original\\us-gdp-growth.xlsx"
 
-us_gdp_growth = pd.read_excel(us_gdp_growth_exl, index_col= 0, header= 0)
+us_gdp_growth = pd.read_excel(us_gdp_growth_exl)
 
-print(us_gdp_growth)
+list = us_gdp_growth.values
+
+years_list = list[0]
+years_list = years_list.tolist()
+years_list.remove('United States')
+
+values_list = list[1]
+values_list = values_list.tolist()
+del values_list[0]
+
+print(years_list)
+print(values_list)
