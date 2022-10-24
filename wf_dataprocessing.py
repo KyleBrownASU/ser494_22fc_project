e9124b7627bdd30346efb936161bffd59b7fe780
KyleBrownASU <97472449+KyleBrownASU@users.noreply.github.com>
2022-10-23 17:19:11 -0700

    added crime_data() method

--- wf_dataprocessing.py
+++ wf_dataprocessing.py
@@ -1,5 +1,7 @@
 import pandas as pd
 
+global_years_names_list = [2000, 2001 ,2002 ,2003 ,2004 ,2005 ,2006 ,2007 ,2008 ,2009 ,2010 ,2011 ,2012 ,2013 ,2014 ,2015 ,2016 ,2017 ,2018 ,2019]
+
 def get_gdp_data():
 
     us_gdp_growth_exl = "data_original\\us-gdp-growth.xlsx"
@@ -16,10 +18,8 @@ def get_gdp_data():
     values_list = values_list.tolist()
     del values_list[0]
 
-    return years_list, values_list
+    return global_years_names_list, values_list
 
-    print(years_list)
-    print(values_list)
 
 
 def unemplyment_data():
@@ -49,10 +49,8 @@ def unemplyment_data():
     y2018 = vals_list[18]
     y2019 = vals_list[19]
     
-
     years_list = [y2000,y2001,y2002,y2003,y2004,y2005,y2006,y2007,y2008,y2009,y2010,y2011,y2012,y2013,y2014,y2015,y2016,y2017,y2018,y2019]
     result_list = []
-
 
     for year in years_list:
         del year[0]
@@ -61,16 +59,51 @@ def unemplyment_data():
         year.append( sum(year) / 12)
         result_list.append(year[12])
 
-    years_names_list = ['2000','2001','2002','2003','2004','2005','2006','2007','2008','2009','2010','2011','2012','2013','2014','2015','2016','2017','2018','2019']
+   
 
-    return years_names_list, result_list
+    return global_years_names_list, result_list
+
+    
+
+def crime_data(input_srt = 'violent' ):
+
+    input_srt = input_srt.lower()
+    crime_exl = 'data_original\\table-1.xls'
+    crime = pd.read_excel(crime_exl, header=3)
+
+    #print(crime.columns.values)
+
+    Population = crime[['Population1']].head(20).values.tolist()
+    Violent_crime = crime[['Violent\ncrime2']].head(20).values.tolist()
+    rape_rate = crime[['Rape\n(legacy \ndefinition) \nrate4']].head(20).values.tolist()
+    robbery_rate = crime[['Robbery \nrate ']].head(20).values.tolist()
+    agg_assult_rate = crime[['Aggravated \nassault rate ']].head(20).values.tolist()
+    prop_crime_rate = crime[['Property \ncrime \nrate ']].head(20).values.tolist()
+
+
+    if 'violent' in input_srt:
+        return global_years_names_list, Violent_crime
+    elif 'rape' in input_srt:
+        return global_years_names_list, rape_rate
+    elif 'robbery' in input_srt:
+        return global_years_names_list, robbery_rate
+    elif 'assult' in input_srt:
+        return global_years_names_list, agg_assult_rate
+    elif 'property' in input_srt:
+        return global_years_names_list, prop_crime_rate
+
+    
+
+   
 
     
 
 
 
 
-
-
 if __name__ == '__main__':
-    unemplyment_data()
+    
+    data, data1 = crime_data()
+    print(data)
+    print(data1)
+    pass
