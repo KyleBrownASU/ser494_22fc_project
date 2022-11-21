bec6b328d7f7addb9385532ca674ae2db364c5fc
KyleBrownASU <97472449+KyleBrownASU@users.noreply.github.com>
2022-11-21 00:29:12 -0700

    added state data class

--- wf_dataprocessing.py
+++ wf_dataprocessing.py
@@ -2,8 +2,14 @@ import os
 import pandas as pd
 import glob
 
-form1 = ['2005', '2006', '2007', '2008', '2009', '2010', '2011', '2012', '2017', '2018', '2019',] # 0,1,2,3,4,9
-form2 = ['2013','2014','2015'] # 0,1,2,3,4,10
+#form1 = ['2005', '2006', '2007', '2008', '2009', '2010', '2011', '2012', '2017', '2018', '2019',] # 0,1,2,3,4,9
+#form2 = ['2013','2014','2015'] # 0,1,2,3,4,10
+
+state_names = ["Alaska", "Alabama", "Arizona", "Arkansas", "California", "Colorado", "Connecticut",
+    "Delaware", "Florida", "Georgia", "Hawaii", "Iowa", "Idaho", "Illinois", "Indiana", "Kansas", "Kentucky", "Louisiana", "Massachusetts",
+    "Maryland", "Maine", "Michigan", "Minnesota", "Missouri", "Mississippi", "Montana", "North Carolina", "North Dakota", "Nebraska", "New Hampshire",
+    "New Jersey", "New Mexico", "Nevada", "New York", "Ohio", "Oklahoma", "Oregon", "Pennsylvania", "Rhode Island", "South Carolina",
+    "South Dakota", "Tennessee", "Texas", "Utah", "Virginia", "Vermont", "Washington", "Wisconsin", "West Virginia", "Wyoming"]
 
 def start():
     path = os.getcwd()
@@ -22,16 +28,21 @@ def start():
             df = pd.read_excel(f, header = 3, usecols=[0,1,2,3,4,9])
         dict = df.to_dict()
 
-        print(f, dict.keys())
+        print(df.shape)
+
+        #print(f, dict.keys())
         
         # print the location and filename
         #print('Location:', f)
         #print('File Name:', f.split("\\")[-1])
         
+
         # print the content
         #print('Content:')
         #print(df)
         #print()
+    
+    #print(len(state_names))
 
     
    
