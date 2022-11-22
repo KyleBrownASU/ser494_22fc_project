564eeb9db480d732723d95a8eb14e17db75928b2
KyleBrownASU <kyle_brown@asu.edu>
2022-11-21 21:41:40 -0700

    save evaulation

---
+++ wf_ml_training.py
@@ -0,0 +1,12 @@
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LinearRegression
+from sklearn.metrics import mean_squared_error, r2_score
+import pandas as pd
+
+def train(x_train, x_test, y_train, y_test):
+    lr = LinearRegression()
+    lr.fit(x_train, y_train)
+    y_train_pred = lr.predict(x_train)
+    y_test_pred = lr.predict(x_test)
+    
+    return y_train_pred, y_test_pred
